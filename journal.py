66c9bb4ad44577a7fc2dfc3698a1674981be19d9
Adam Greig <adam@adamgreig.com>
2011-03-04 20:03:34 +0000

    shows/stores posts

--- journal.py
+++ journal.py
@@ -2,26 +2,55 @@
 # A simple journal web application.
 # Adam Greig, 2011. All code released into the public domain.
 
+import os
+import time
+import datetime
+from hashlib import sha256
 from flask import Flask
 from flask import request, session, redirect, url_for, render_template, flash
-
-from hashlib import sha256
 
 app = Flask(__name__)
 app.config.from_object('password')
 app.config.from_object('config')
 
+def make_posts_dir_if_not_exists():
+    """Makes the posts/ directory if it doesn't exist."""
+    if "posts" not in os.listdir("."):
+        os.mkdir("./posts")
+
+def get_posts():
+    """Returns a list of post dicts from the posts/ folder."""
+    make_posts_dir_if_not_exists()
+    posts = []
+    files = reversed(sorted(os.listdir("posts/")))
+    for filename in files:
+        date = datetime.datetime.fromtimestamp(float(filename))
+        new_post = {'date': date.strftime("%H:%M, %A %d %B %Y")}
+        with open("posts/" + filename) as f:
+            new_post['body'] = f.read()
+        posts.append(new_post)
+    return posts
+
+def store_post(post):
+    """Saves post text to a datestamped file."""
+    make_posts_dir_if_not_exists()
+    with open("posts/" + str(int(time.time())), "w") as f:
+        f.write(post)
+
 @app.route('/')
 def index():
     """List the most recent posts, with a text box for a new post."""
-    posts = [
-        {'date': '4th March 2011', 'body': 'this is a test'}
-    ]
+    try:
+        posts = get_posts()
+    except (ValueError, TypeError, KeyError, OSError, IOError):
+        posts = []
+    posts = get_posts()
     return render_template('index.html', posts=posts)
 
 @app.route('/post', methods=['POST'])
 def post():
     """Stores a new post."""
+    store_post(request.form['post_body'])
     flash("Post added.")
     return redirect(url_for("index"))
 
